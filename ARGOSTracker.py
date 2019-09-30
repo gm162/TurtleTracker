844637912f7ce982abac119a8131ceb9d0a6cd50
gm162 <54636161+gm162@users.noreply.github.com>
2019-09-30 12:11:23 -0400

    Adds front matter

---
+++ ARGOSTracker.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Parses a line of ARGOS tracking data 
+#
+# Created by: Gia Mancini (gia.mancini@duke.edu)
+# Created on: Fall 2019
+#--------------------------------------------------------------
